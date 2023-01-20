ff70eaac0be7277f56c8d3be16e4953660fc586e
youngeunkk <duddms0939@gmail.com>
2023-01-20 21:05:52 +0900

    모드 전환 및 함수, 변수 변경

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import './App.css';
 import { useState } from 'react';
 
 
-function Freeweight({onChangeMode, onChangeBench, onChangeDead, onChangeSquat}) {
+function Freeweight({onCreate}) {
 
   return (
 
     <div className="freeweight">
       <div className="freeweightBox">
         <h2>3대 중량을 입력해주세요!</h2>
-        <ul>
-          <li>스쿼트 <input type="text" id="squat" name="squat" maxLength="3" size="4"
-            onChange={onChangeSquat}></input> kg</li>
-          <li>벤치프레스 <input type="text" id="benchpress" name="benchpress" maxLength="3" size="4"
-            onChange={onChangeBench}
-          ></input> kg</li>
-          <li>데드리프트 <input type="text" id="deadlift" name="deadlift" maxLength="3" size="4"
-            onChange={onChangeDead}></input> kg</li>
-        </ul>
-        <button type="submit" id="sbd" name="sbd" onClick={onChangeMode}>확인</button>  {/* setMode 변환예정 */}
+        <form onSubmit={onCreate}>
+          <ul>
+            <li>스쿼트 <input type="text" id="squat" name="squat" maxLength="3" size="4"
+              ></input> kg</li>
+            <li>벤치프레스 <input type="text" id="benchpress" name="benchpress" maxLength="3" size="4"
+              
+            ></input> kg</li>
+            <li>데드리프트 <input type="text" id="deadlift" name="deadlift" maxLength="3" size="4"
+              ></input> kg</li>
+          </ul>
+          <button type="submit" id="sbd" name="sbd">확인</button>
+        </form>
       </div>
     </div>
   )
@@ -26,11 +28,14 @@ function Freeweight({onChangeMode, onChangeBench, onChangeDead, onChangeSquat}) 
 
 function Hello(props) {
 
+  const sum = props.squat + props.benchpress + props.deadlift;
+
   return (
     <div className="hello">
-      <h4>    스쿼트 : {props.inputSquat} </h4>
-      <h4>벤치프레스 : {props.inputBenchpress} </h4>
-      <h4>데드리프트 : {props.inputDeadlift} </h4>
+      <h4>스쿼트 : {props.squat}</h4>
+      <h4>벤치프레스 : {props.benchpress}</h4>
+      <h4>데드리프트 : {props.deadlift}</h4>
+      <h3>3대 중량 : {sum}</h3>
     </div>
 
   )
@@ -39,58 +44,36 @@ function Hello(props) {
 
 function App() {
 
-  let [inputSquat, setInputSquat] = useState('');
-  let [inputBenchpress, setInputBenchpress] = useState('');
-  let [inputDeadlift, setInputDeadlift] = useState('');
-
   let [mode, setMode] = useState('Freeweight');
-
   let content = null;
 
-  const onChangeSquat = (e) => {
-    if (isNaN(e.target.value) === true) {
-      alert("숫자만 입력해주세요!");
-      e.target.value = '';
-    } else {
-      setInputSquat(e.target.value);
-    }
-  }
-
-  const onChangeBench = (e) => {
-    if (isNaN(e.target.value) === true) {
-      alert("숫자만 입력해주세요!");
-      e.target.value = '';
-    } else {
-      setInputBenchpress(e.target.value);
-    }
-  }
-
-  const onChangeDead = (e) => {
-    if (isNaN(e.target.value) === true) {
-      alert("숫자만 입력해주세요!");
-      e.target.value = '';
-    } else {
-      setInputDeadlift(e.target.value);
-    }
-  }
+  let [squat, setSquat] = useState();
+  let [benchpress, setBenchpress] = useState();
+  let [deadlift, setDeadlift] = useState();
 
   const onChangeMode = () => {
     setMode('Hello');
   }
 
+  const onCreate = (e) => {
+    
+    if (e.target.squat.value === '' || e.target.benchpress.value === '' || e.target.deadlift.value === '') {
+      alert('빈칸을 입력해주세요!!');
+      e.preventDefault();
+    } else {
+      setSquat(Number(e.target.squat.value));
+      setBenchpress(Number(e.target.benchpress.value));
+      setDeadlift(Number(e.target.deadlift.value));
+      onChangeMode();
+      e.preventDefault();
+    }
+  }
 
   if (mode === 'Freeweight') {
-    content = <Freeweight 
-        onChangeMode={onChangeMode} 
-        onChangeSquat={onChangeSquat}
-        onChangeBench={onChangeBench}
-        onChangeDead={onChangeDead} />
+    content = <Freeweight onCreate={onCreate} />
   } else if (mode === 'Hello') {
-    content = <Hello 
-        inputSquat={inputSquat} 
-        inputBenchpress={inputBenchpress} 
-        inputDeadlift={inputDeadlift}/>
-      }
+    content = <Hello squat={squat} benchpress={benchpress} deadlift={deadlift} />
+  }
 
 
   return (
