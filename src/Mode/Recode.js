97744ee54d67c8bc84f232097af9bfc6adea5eda
youngeunkk <duddms0939@gmail.com>
2023-03-06 16:10:23 +0900

    id값 추가하기 (코드 수정)

--- src/Mode/Recode.js
+++ src/Mode/Recode.js
@@ -1,24 +1,32 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { onCreateRecode } from "../Slice/recodeSlice";
 
 
 function Recode(props) {
 
-    const [date, setDate] = useState('');
+    const recode = useSelector(state => {
+        return state.recode;
+    })
 
+    const [date, setDate] = useState('');
     const [body, setBody] = useState('');
 
     const dispatch = useDispatch();
 
+
     const onCreate = (e) => {
         if (e.target.date.value === '' || e.target.body.value === '') {
             alert('날짜 선택과 글을 작성해주세요');
-            e.preventDefault();        
+            e.preventDefault();
         } else {
             e.preventDefault();
-            const finalResult = Object.assign(date, body);
-            dispatch(onCreateRecode(finalResult));
+            let data = {
+                id: recode.length + 1,
+                date: date,
+                body: body
+            }
+            dispatch(onCreateRecode(data));
             alert('글이 작성되었습니다');
             props.onChangeHomeMode();
         }
@@ -27,20 +35,16 @@ function Recode(props) {
     return (
         <div className="recodeArea">
             <form onSubmit={onCreate}>
-                <p><input type="date" name="date" id="date" onChange={(e)=>{
-                    let date2 = {...date};
-                    date2 = {date : e.target.value};
-                    setDate(date2)
+                <p><input type="date" name="date" id="date" onChange={(e) => {
+                    setDate(e.target.value)
                 }}></input></p>
-                <p><textarea name="body" id="textarea" cols="80" row="40" maxLength="300" placeholder="운동일지를 적어주세용" onChange={(e)=>{
-                    let body2 = {...body};
-                    body2 = {body : e.target.value};
-                    setBody(body2)
+                <p><textarea name="body" id="textarea" cols="80" row="40" maxLength="300" placeholder="운동일지를 적어주세용" onChange={(e) => {
+                    setBody(e.target.value)
                 }}></textarea></p>
                 <button type="submit">저장</button>
             </form>
             <div>
-                
+
             </div>
         </div>
     )
