d1dc6f81be1687a6bfa95832e75ee1b709feca19
youngeunkk <duddms0939@gmail.com>
2023-03-14 11:27:41 +0900

    Home.js footer 추가 등 UI 변경

--- src/Mode/Home.js
+++ src/Mode/Home.js
@@ -3,8 +3,6 @@ import image1 from "../img/image1.jpg"
 import image2 from "../img/image2.jpg"
 import image3 from "../img/image3.jpg"
 import image4 from "../img/image4.jpg"
-import image5 from "../img/image5.jpg"
-import image6 from "../img/image6.jpg"
 
 
 
@@ -13,36 +11,42 @@ function Home() {
     const navigate = useNavigate();
 
     const images = [
-        { id: 1, src: image1 , alt : "1"},
-        { id: 2, src: image2 , alt : "2"},
-        { id: 3, src: image3 , alt : "3"},
-        { id: 4, src: image4 , alt : "4"},
-        { id: 5, src: image5 , alt : "5"},
-        { id: 6, src: image6 , alt : "6"},
+        { id: 1, src: image1, alt: "1" },
+        { id: 2, src: image2, alt: "2" },
+        { id: 3, src: image3, alt: "3" },
+        { id: 4, src: image4, alt: "4" },
     ];
 
     return (
         <div className="home">
             <h1>POWER AND BUILDING</h1>
             <h2>강력하고 건강하게!</h2>
-            <p> 웨이트 트레이닝에서 가장 중요한 3대 운동(스쿼트, 벤치프레스, 데드리프트)을 통해 
+            <p> 웨이트 트레이닝에서 가장 중요한 3대 운동(스쿼트, 벤치프레스, 데드리프트)을 통해
                 몸을 보다 더 강력하고 건강하게 가꾸어 나가고 수치 변화를 매번 기록하고 피드백하며
                 보다 더 빠르게 성장하는 것을 목표로 합니다.
             </p>
-
-            <button onClick={() => {
-                navigate('/mypage');
-            }}>마이페이지</button>
-             <button onClick={() => {
-                navigate('/login');
-            }}>로그인</button>
-
-                <div className="images">
-                    {images.map((image) => (
-                        <img key={image.id} src={image.src} alt={image.alt} />
-                    ))}
-                </div>
-            
+            <div className="btnarea">
+                <button id="mypagebtn" onClick={() => {
+                    navigate('/mypage');
+                }}>My Page</button>
+                <button id="loginbtn" onClick={() => {
+                    navigate('/login');
+                }}>LOG IN</button>
+            </div>
+            <div className="images">
+                {images.map((image) => (
+                    <img
+                        key={image.id}
+                        src={image.src}
+                        alt={image.alt}
+                    />
+                ))}
+            </div>
+            <div className="footerparent">
+                <footer>
+                        instagram : xpkye
+                </footer>
+            </div>          
         </div>
     )
 }
